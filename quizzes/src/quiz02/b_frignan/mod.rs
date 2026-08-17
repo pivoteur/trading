bfce7fbfaebe314632bbf05be5924097173f83c3
B-Frenchie <bp.pivottech1@gmail.com>
2026-08-17 14:16:07 -0400

    bug fixes squashed and mangled 2026-08-17

--- quizzes/src/quiz02/b_frignan/mod.rs
+++ quizzes/src/quiz02/b_frignan/mod.rs
@@ -35,7 +35,9 @@ struct Args {
 pub async fn runoff_with_args() -> ErrStr<()> {
     let blockchains: HashMap<String, String> =
        [("binance", "bsc") ].into_iter().map(|(a,b)| (s(a), s(b))).collect();
-    fn lookup(h: &HashMap<String, String>) -> impl Fn(&str) -> String { move |k| h.get(k).unwrap().to_string() }
+    fn lookup(h: &HashMap<String, String>) -> impl Fn(&str) -> String + '_ {
+        move |k| h.get(k).cloned().unwrap_or_else(|| k.to_string())
+    }
   let args = parse_args_add_banner!(Args);
   let wallet_addy = get_env("WALLET_ADDRESS")?;
   let keystore_addy = get_env("TVA_KEYSTORE_PATH")?;
@@ -43,6 +45,6 @@ pub async fn runoff_with_args() -> ErrStr<()> {
   let registry = load_token_registry(&tokens)?;
   let block = lookup(&blockchains);
   let ans = attempt_trade_with_actual_amount(&block(&args.blockchain), &wallet_addy, &registry, &args.from_token, "USDC", args.amount, 0.0, 1000, &keystore_addy, true, args.debug).await;
-  println!("answer is {ans:?}");
+  println!("I gotch you, {ans:?}");
   Ok(())
 }
