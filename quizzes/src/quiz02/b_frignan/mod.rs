f1f1095dbe69b2ba526e753ac968c17d3ead193f
B-Frenchie <bp.pivottech1@gmail.com>
2026-08-27 10:35:07 -0400

    frignan PR corrections 2026-08-27

--- quizzes/src/quiz02/b_frignan/mod.rs
+++ quizzes/src/quiz02/b_frignan/mod.rs
@@ -1,15 +1,18 @@
 use std::{collections::HashMap, println};
 use trading::auto_trading::{
-            AttemptOutcome::DryRunWouldClear, TokenRegistry, attempt_trade_with_actual_amount, parse_token_registry
+                AttemptOutcome::DryRunWouldClear, 
+                TokenRegistry, 
+                attempt_trade_with_actual_amount, 
+                parse_token_registry
 };
 use book::{
-   parse_args_add_banner,
-   cli_utils::generate_banner,
-   err_utils::ErrStr,
-   string_utils::{UppercaseString,s},
-   utils::get_env,
-   file_utils::read_file,
-   currency::usd::mk_usd
+        parse_args_add_banner,
+        cli_utils::generate_banner,
+        err_utils::ErrStr,
+        string_utils::{UppercaseString, s},
+        utils::get_env,
+        file_utils::read_file,
+        currency::usd::mk_usd
 };
 use clap::Parser;
 
@@ -22,17 +25,16 @@ pub fn load_token_registry(tokens: &str) -> ErrStr<TokenRegistry> {
 //=========================================================================================
 #[derive(Debug, Parser)]
 #[command(name = "frignan")]
-#[command(version = "1.0.5")]
+#[command(version = "1.1.0")]
 struct Args {
     from_token: UppercaseString,
-    amount: f64,
     blockchain: String,
 
     #[arg(short, long)]
     debug: bool
 }
 
-async fn runoff_continuation(blockchain: &str, from_token: &str, amount: f64, debug: bool) -> ErrStr<()> {
+async fn runoff_continuation(blockchain: &str, from_token: &str, debug: bool) -> ErrStr<()> {
     let blockchains: HashMap<String, String> =
        [("binance", "bsc") ].into_iter().map(|(a,b)| (s(a), s(b))).collect();
     fn lookup(h: &HashMap<String, String>) -> impl Fn(&str) -> String + '_ {
@@ -43,7 +45,7 @@ async fn runoff_continuation(blockchain: &str, from_token: &str, amount: f64, de
     let tokens = read_file(&format!("data/{}.toml", blockchain))?;
     let registry = load_token_registry(&tokens)?;
     let block = lookup(&blockchains);
-    let ans = attempt_trade_with_actual_amount(&block(&blockchain), &wallet_addy, &registry, &from_token, "USDC", amount, 0.0, 1000, &keystore_addy, true, debug).await?;
+    let ans = attempt_trade_with_actual_amount(&block(&blockchain), &wallet_addy, &registry, &from_token, "USDC", 1.0, 0.0, 1000, &keystore_addy, true, debug).await?;
     if let DryRunWouldClear{quoted_amount_out} = ans{
         let price = mk_usd(quoted_amount_out as f32);
             println!("{from_token}'s price is {price}");
@@ -55,7 +57,7 @@ async fn runoff_continuation(blockchain: &str, from_token: &str, amount: f64, de
 
 pub async fn runoff_with_args() -> ErrStr<()> {
   let args = parse_args_add_banner!(Args);
-  runoff_continuation(&args.blockchain, &args.from_token, args.amount, args.debug).await
+  runoff_continuation(&args.blockchain, &args.from_token, args.debug).await
 }
 
 //=========================================================================
@@ -102,7 +104,7 @@ mod unit_tests {
 //=========================================================================
 #[cfg(not(tarpaulin_include))]
 #[cfg(test)]
-pub mod functional_test { 
+pub mod functional_test {
     use super::*;
     use paste::paste;
     use book::{ create_testing, utils::now };
@@ -111,10 +113,10 @@ pub mod functional_test {
     create_testing!("quiz02::b_frignan");
 
     run!("frignan_functionailty", {
-        now(runoff_continuation("avalanche", "BTC", 1.0, true))?
+        now(runoff_continuation("avalanche", "BTC", true))?
     });
 
     run!("frignan_undead", {
-        now(runoff_continuation("avalanche", "UNDEAD", 1.0, false))?
+        now(runoff_continuation("avalanche", "UNDEAD", false))?
     });
 }
