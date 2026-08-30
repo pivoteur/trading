4ccceb2d68b0e5f5946406bb4a40c486cfa8f887
pivoteur <dauclair@hotmail.com>
2026-08-30 02:43:29 -0400

    importing protocol/libs functionality

--- libs/src/log_utils.rs
+++ libs/src/log_utils.rs
@@ -1,13 +1,20 @@
 use book::{ debug, not_implemented, err_utils::ErrStr };
+use libs::{
+   fetchers::quotes::fetch_quotes,
+   types::{ blockchains::Blockchain, pools::Pool }
+};
+
 // use super::auto_trading::{
    // query_swap,
 
-pub async create_log(path: &str, blockchain: &str,
+pub async create_log(path: &str, wallet: &str, blockchain: &Blockchain,
                      primary: &str, amt: f64, pivot: &str,
                      dry_run: bool, debug: bool) -> ErrStr<()> {
    debug!("libs::log_utils::create_log");
    not_implemented!("create_log)
 }
+
+fn pool_file_name(wallet: &str, p: &Pool, 
 
 /*
 pub async fn query_swap(
